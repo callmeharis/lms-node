e0e0257f1dbc07293fe11adf8fae9b2345a15621
callmeharis <theharis99@gmail.com>
2024-11-25 13:23:16 +0500

    initial structure

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
+const port = process.env.PORT;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is up and listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
