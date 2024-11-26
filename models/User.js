105965871b23c0172932ac0916e5b05a5474b958
callmeharis <theharis99@gmail.com>
2024-11-26 14:23:49 +0500

    complete auth system

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const AuthSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please provide name"],
+      minLength: 3,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "please provide email"],
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minLength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "instructor", "student"],
+      default: "student",
+    },
+  },
+  { timestamps: true }
+);
+
+AuthSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+AuthSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", AuthSchema);
